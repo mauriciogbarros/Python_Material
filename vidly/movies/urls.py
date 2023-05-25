a4b9609070019ceb24c416cf056bf22b8212e40c
mauriciogbarros <63826532+mauriciogbarros@users.noreply.github.com>
2023-05-25 09:15:30 -0400

    Add files via upload

---
+++ vidly/movies/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# /movies/1
+
+app_name = 'movies'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
+]
